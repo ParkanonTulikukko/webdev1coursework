40781b20b682991b17582323ed43a218d972c9c9
Jaakko Kankaansivu <jaakko.kankaansivu@tuni.fi>
2020-11-11 02:51:32 +0200

    Exercise 9.4

--- models/user.js
+++ models/user.js
@@ -2,8 +2,51 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+function hash(password) {
+  if (password.length < 10) {
+     return password;
+  } else {
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(password, salt);
+    return hash;
+  }
+  
+}
+
 const userSchema = new Schema({
   // TODO: 9.4 Implement this
+  name: {
+    type: String,
+    minlength: 1,
+    maxlength: 50,
+    required: true,
+    //match: /^\S.*[^.\s]$/,
+    trim: true
+  },
+	email: {
+    type: String,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    minlength: 10,
+    required: true,
+    set: hash
+    //bcrypt.genSaltSync(10, (err, salt) => {
+    //  bcrypt.hash(v, salt, (err, hash) => {
+    //    v = hash;
+    //  });
+    //})
+  },
+  role: {
+    type: String,
+    enum: ["admin", "customer"],
+    default: "customer",
+    trim: true,
+    lowercase: true
+  }
 });
 
 /**
@@ -14,6 +57,7 @@ const userSchema = new Schema({
  */
 userSchema.methods.checkPassword = async function (password) {
   // TODO: 9.4 Implement this
+  return bcrypt.compare(password, this.password);
 };
 
 // Omit the version key when serialized to JSON
